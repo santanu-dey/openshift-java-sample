3f71e28207e0199dc09cafebbc592e04e9b59f1f
Santanu Dey <dey.santanu@gmail.com>
2015-11-19 14:47:58 +0800

    JSONML

--- src/main/java/com/wsg/util/ConvertPayload.java
+++ src/main/java/com/wsg/util/ConvertPayload.java
@@ -1,7 +1,10 @@
 package com.wsg.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.json.*;
 
 import javax.servlet.ServletException;
@@ -9,6 +12,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 /**
  * Servlet implementation class ConvertPayload
@@ -42,17 +58,74 @@ public class ConvertPayload extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String responseBody =  request.getParameter("code");
 		String soapmessageString = 	request.getParameter("code");
-		JSONObject soapDatainJsonObject = new JSONObject();
+		JSONObject soapDatainJsonObject = null;
 		String stringXML ="";
+		Document xmlDoc= null;
 		try {
-			soapDatainJsonObject = XML.toJSONObject(soapmessageString);
-			stringXML = XML.toString(soapDatainJsonObject);
-		}catch (Exception e){
+			soapDatainJsonObject = JSONML.toJSONObject(soapmessageString);
+			stringXML = JSONML.toString(soapDatainJsonObject);
 			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			
+			ByteArrayInputStream stream = new ByteArrayInputStream(stringXML.getBytes());
+			xmlDoc = dBuilder.parse(stream);
+			xmlDoc.getDocumentElement().normalize();
+			System.out.println("hello\n\n\n");
+			NodeList nodeList = xmlDoc.getElementsByTagName("*");
+			System.out.println("Length of nodes ="+nodeList.getLength());
+		    for (int i = 0; i < nodeList.getLength(); i++) {
+		        Node node = nodeList.item(i);
+		        System.out.println(node.getNodeName());
+		        if (node.getNodeType() == Node.ELEMENT_NODE) {
+		            // do something with the current element
+		            if (node.getNodeName().toLowerCase().startsWith("xmlns:"))
+		            {
+		            	// add attribute 
+		            	Element parentElement = (Element) node.getParentNode();
+		            	parentElement.setAttribute(node.getNodeName(), node.getTextContent());
+		            	parentElement.removeChild(node);
+		            }
+		            
+		        }
+		    }
+			
+			
+		}catch (Exception e){
+			e.printStackTrace();
+			stringXML = e.getMessage() + e.toString();
 		}
 		PrintWriter out = response.getWriter();
-		out.print(soapDatainJsonObject.toString(4) +"\n\n"+stringXML);
+		String xmlString = "Could not convert document back to XML. Please check input error or conversion error:\n";
+		if (xmlDoc!=null && soapDatainJsonObject!=null){
+			try {
+				xmlString= "Re-converted back into XML: \n\n "+getXMLString(xmlDoc);
+			} catch (TransformerException e) {
+				// TODO Auto-generated catch block
+				xmlString = xmlString+ e.getMessage() + e.toString();
+				e.printStackTrace();
+			}
+		
+			out.print("Converted JSON format:\n\n"+soapDatainJsonObject.toString(4)+"\n\n"+xmlString);
+		}else{
+			out.print("Could not convert document. Please check document format. Caused by:\n"+stringXML);
+		}
+		
 		out.flush();
+	}
+	public static String getXMLString(Document doc) throws TransformerException{
+		TransformerFactory tf = TransformerFactory.newInstance();
+		Transformer transformer = tf.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		//String output = writer.getBuffer().toString().replaceAll("\n|\r", "");
+		String output = writer.getBuffer().toString();
+		return output;
 	}
 
 }
