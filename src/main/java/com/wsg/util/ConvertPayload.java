da3ac131a1f9d892142bbc5a324c711893b08d3f
Santanu Dey <dey.santanu@gmail.com>
2015-11-19 16:01:35 +0800

    lib

--- src/main/java/com/wsg/util/ConvertPayload.java
+++ src/main/java/com/wsg/util/ConvertPayload.java
@@ -47,7 +47,8 @@ public class ConvertPayload extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String responseBody = 
 				"<!DOCTYPE html>\r\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/css/bootstrap.min.css\"/>\r\n<html>\r\n\r\n<head>\r\n<style type=\"text/css\" media=\"screen\">\r\n    #editor { \r\n/*        position: absolute;\r\n        top: 0;\r\n        right: 0;\r\n        bottom: 0;\r\n        left: 0;*/\r\n        min-height: 400px;\r\n        height:auto;\r\n    }\r\n</style>\r\n\r\n</head>\r\n\r\n<body>\r\n\r\n\r\n<!-- <div id=\"editor\">\r\n<p><doc>paste your xml here</doc></p>\r\n</div>\r\n -->\r\n\r\n <div class=\"container\">\r\n    <div class=\"panel panel-default\">\r\n        <div class=\"panel-heading\">\r\n             <span class=\"panel-title\">Code Converter</span>\r\n        </div>\r\n        <div class=\"panel-body\">\r\n            <div class=\"row\">                \r\n                <div class=\"col-md-10\">\r\n                    <div id=\"editor\">\r\n                    <p><doc>Paste your xml here</doc></p>\r\n                    </div>\r\n                </div>\r\n            </div>\r\n        </div>\r\n    </div>\r\n    \r\n    <div class=\"text-center\"></div>\r\n    \r\n<form id=\"codeform\"  method=\"post\" action=\"convert-payload\">\r\n<input type=\"hidden\" name=\"code\" id=\"code\" value=\"\" />\r\n<input type=\"submit\" value=\"Convert\" onclick=\"getVal();\">\r\n</form>\r\n    <br/>\r\n    <br/>\r\n</div> \r\n\r\n<script src=\"https://ace.c9.io/build/src/ace.js\" type=\"text/javascript\" charset=\"utf-8\"></script>\r\n<script type=\"text/javascript\">\r\n   var editor = ace.edit(\"editor\");\r\n   // editor.setTheme(\"ace/theme/twilight\");\r\n   editor.setTheme(\"ace/theme/monokai\");\r\n   editor.getSession().setMode(\"ace/mode/xml\");\r\n\r\n\r\n    function getVal()\r\n    {\r\n      var editor= ace.edit(\"editor\"); \r\n      var input = document.getElementById('code');\r\n      // this line is necessary\r\n      // \"editor\" is the id of the ACE editor div\r\n      input.value =editor.getSession().getValue();\r\n      return true;\r\n\r\n    }\r\n</script>\r\n\r\n</body>\r\n</html>";
-				PrintWriter out = response.getWriter();
+		response.setContentType("text/html");		
+		PrintWriter out = response.getWriter();
 		out.print(responseBody);
 		out.flush();
 	}
@@ -95,6 +96,8 @@ public class ConvertPayload extends HttpServlet {
 			e.printStackTrace();
 			stringXML = e.getMessage() + e.toString();
 		}
+		
+		response.setContentType("text/plain");
 		PrintWriter out = response.getWriter();
 		String xmlString = "Could not convert document back to XML. Please check input error or conversion error:\n";
 		if (xmlDoc!=null && soapDatainJsonObject!=null){
